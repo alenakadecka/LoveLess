08cd1a018bba3d1be923ba54591ebea3e106ad50
Alena Kadecká <kadecka.alena@gmail.com>
2024-11-09 21:58:29 +0100

    delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p className="headline">Love Less</p>
+        <p className="title-description">
+          Your guide to a sustainable lifestyle.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Dear visitor, welcome at Webpage about your new life-style. You can
+          find here information about minimalism, ecology and package-free ways
+          of living.
+        </p>
       </header>
+
+      <div className="buttons">
+        <div className="button">Package-free shops around</div>
+        <div className="button">Differencies in Separation </div>
+        <div className="button">Test Your Life-style</div>
+      </div>
     </div>
   );
 }
