3be1542d4920db992f1f62b007a6e605d0ec7378
Alena Kadecká <kadecka.alena@gmail.com>
2025-01-23 15:11:55 +0100

    questions added

--- src/Pages/TestYourLifeStylePage/Questions.ts
+++ src/Pages/TestYourLifeStylePage/Questions.ts
@@ -174,4 +174,116 @@ export const questions: Question[] = [
       },
     ],
   },
+  {
+    question: 'Do you own a fruit or vegetable net?',
+    weight: 1,
+    answers: [
+      {
+        text: 'Yes, of course, I love it.',
+        value: 1,
+      },
+      {
+        text: '.No, but I wish to buy it soon.',
+        value: 0.5,
+      },
+      {
+        text: 'No, and I do not even plan to own it.',
+        value: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'Do you use solid mind, shampoo and conditioner instead of the ones in plastic packaging?',
+    weight: 1,
+    answers: [
+      {
+        text: 'Yes, I prefer to use only these solid ones.',
+        value: 1,
+      },
+      {
+        text: 'I used both types of cosmetics.',
+        value: 0.5,
+      },
+      {
+        text: 'No, I use still the plastic ones.',
+        value: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'Do you believe that even an individual can help change nature and the planet with his lifestyle ?',
+    weight: 1,
+    answers: [
+      {
+        text: 'Yes, of course. I beleive in my individual effort.',
+        value: 1,
+      },
+      {
+        text: 'I often doubt it, when I see the behavior of the others.',
+        value: 0.5,
+      },
+      {
+        text: 'No, thats one of the reason, why I am often demotivated to separate and for zero waste.',
+        value: 0,
+      },
+    ],
+  },
+  {
+    question: 'What do you do with unwanted and unusable gifts from Christmas?',
+    weight: 1,
+    answers: [
+      {
+        text: 'I try to donate or swap them. Find them a new home.',
+        value: 1,
+      },
+      {
+        text: 'I maintend them until I am forced to do something in case of moving for example.',
+        value: 0.5,
+      },
+      {
+        text: 'I simply throw them away, without using energy to find some purpose of usage or donation of them.',
+        value: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'When a store offers the option to return packaging from products for a discount on a future purchase, do you make a habit of collecting and returning the packaging?',
+    weight: 1,
+    answers: [
+      {
+        text: 'Yes, always. I even choose these companies to shop with preference.',
+        value: 1,
+      },
+      {
+        text: 'I use it sometimes, but not purposly.',
+        value: 0.5,
+      },
+      {
+        text: 'I do not even know, that something like this exists.',
+        value: 0,
+      },
+    ],
+  },
+  {
+    question:
+      'Do you have things at home that are just gathering dust and you have not touched them in a year? Not even to wipe the dust off ?',
+    weight: 1,
+    answers: [
+      {
+        text: 'No, I am reducing all things around often and persistantly.',
+        value: 1,
+      },
+      {
+        text: 'Yes, a few, that am not able to give away. However, the dust is already removed.',
+        value: 0.5,
+      },
+      {
+        text: 'Yes a lot. I do not feel motivated to give them away even if, I do not need them at all.',
+        value: 0,
+      },
+    ],
+  },
 ];
